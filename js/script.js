6615b7f86337744e419eff6ee85dd4d239ff81b5
albertiniclaudia <claudia.albertini@live.it>
2022-05-10 17:02:03 +0200

    primo commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var domanda1 = prompt('4 + 3');
-var domanda2 = prompt('3 - 1');
-var domanda3 = prompt('1 * 4');
-var domanda4 = prompt('4 / 2');
+var domanda1 = prompt('4 + 3 =');
+var domanda2 = prompt('3 - 1 =');
+var domanda3 = prompt('1 * 4 =');
+var domanda4 = prompt('4 / 2 =');
 document.getElementById('domanda1').innerHTML = domanda1;
 document.getElementById('domanda2').innerHTML = domanda2;
 document.getElementById('domanda3').innerHTML = domanda3;
@@ -12,25 +12,43 @@ document.write( 'Ecco le tue risposte: ' + fullName);
 console.log( 'Ecco le tue risposte: ' + fullName);
 
 if (domanda1 == 7) {
-    document.write = 7;
+    document.getElementById('domanda1').innerHTML = '7';
+    console.log(domanda1 + ' ');
 }
 else {
-    document.write = 'sbagliato';
+    document.getElementById('domanda1').innerHTML = 'Hai sbagliato';
+    console.log(domanda1 + ' ' + 'Hai sbagliato');
 }
 
-/* var somma = 'più';
-document.getElementById('più').innerHTML = somma;
+if (domanda2 == 2) {
+    document.getElementById('domanda2').innerHTML = '2';
+    console.log(domanda2 + ' ');
+}
+else {
+    document.getElementById('domanda2').innerHTML = 'Hai sbagliato';
+    console.log(domanda2 + ' ' + 'Hai sbagliato');
+}
 
-var differenza = 'meno';
-document.getElementById('meno').innerHTML = differenza;
+if (domanda3 == 4) {
+    document.getElementById('domanda3').innerHTML = '4';
+    console.log(domanda1 + ' ');
+}
+else {
+    document.getElementById('domanda3').innerHTML = 'Hai sbagliato';
+    console.log(domanda3 + ' ' + 'Hai sbagliato');
+}
 
-var moltiplicazione = 'per';
-document.getElementById('per').innerHTML = moltiplicazione;
+if (domanda4 == 2) {
+    document.getElementById('domanda4').innerHTML = '2';
+    console.log(domanda1 + ' ');
+}
+else {
+    document.getElementById('domanda4').innerHTML = 'Hai sbagliato';
+    console.log(domanda4 + ' ' + 'Hai sbagliato');
+}
 
-var divisione = 'diviso';
-document.getElementById('diviso').innerHTML = divisione;
 
-const n1 = '1';
+/* const n1 = '1';
 const n2 = '2';
 const n3 = '3';
 const n4 = '4';
